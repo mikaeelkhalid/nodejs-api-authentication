fc7a9b35cbab83529b43c0f3027c24bb059cb132
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-10-25 15:48:13 +0500

    handle http errors

--- app.js
+++ app.js
@@ -5,6 +5,26 @@ require('dotenv').config();
 
 const app = express();
 
+app.get('/', async (req, res) => {
+  await res.status(200).json({
+    message: 'hello world',
+  });
+});
+
+app.use(async (req, res, next) => {
+  await next(createError.NotFound());
+});
+
+app.use(async (err, req, res, next) => {
+  await res.status(err.status || 500);
+  await res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
