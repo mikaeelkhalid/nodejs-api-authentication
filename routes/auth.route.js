135a8bdb7ba3ce31d06b8973b29304996f0dcbca
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-10-27 00:43:00 +0500

    validate register user: http error

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const createError = require('http-errors');
 
-router.post('/register', async (req, res) => {
-  res.send('register');
+router.post('/register', async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      throw createError.BadRequest('Email and password are required');
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post('/login', async (req, res) => {
