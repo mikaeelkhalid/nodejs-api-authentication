35f6fcf13e62501fdc6f1525263e6639f2d71319
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-10-27 01:27:12 +0500

    validate register user using Joi

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,22 +2,29 @@ const express = require('express');
 const router = express.Router();
 const createError = require('http-errors');
 const User = require('../models/user.model');
+const { authSchema } = require('../helpers/validationSchema');
 
 router.post('/register', async (req, res, next) => {
   try {
-    const { email, password } = req.body;
-    if (!email || !password) {
-      throw createError.BadRequest('Email and password are required');
-    }
+    const result = await authSchema.validateAsync(req.body);
+
+    const { email, password } = result;
 
     const doesExist = await User.findOne({ email });
+
     if (doesExist) {
-      throw createError.Conflict('Email already exists');
+      throw createError.Conflict(`${email} already exists`);
     }
+
     const user = new User({ email, password });
+
     const savedUser = await user.save();
+
     res.send(savedUser);
   } catch (error) {
+    if (error.isJoi === true) {
+      error.status = 422;
+    }
     next(error);
   }
 });
