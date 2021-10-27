f172c2ba093d5d86910873e4c6ce7942417c5e43
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-10-28 01:56:37 +0500

    verifying access token using middleware

--- helpers/jwtHelper.js
+++ helpers/jwtHelper.js
@@ -9,7 +9,7 @@ module.exports = {
       const secret = process.env.ACCESS_TOKEN_SECRET;
 
       const options = {
-        expiresIn: '1h',
+        expiresIn: '20s',
         issuer: 'mikaeelkhalid.github.io',
         audience: userId,
       };
@@ -22,6 +22,26 @@ module.exports = {
           resolve(token);
         }
       });
+    });
+  },
+  verifyAccessToken: (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+
+    if (!authHeader) {
+      return next(createError.Unauthorized());
+    }
+
+    const bearerToken = authHeader.split(' ');
+
+    const token = bearerToken[1];
+
+    JWT.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
+      if (err) {
+        return next(createError.Unauthorized());
+      }
+
+      req.payload = payload;
+      next();
     });
   },
 };
