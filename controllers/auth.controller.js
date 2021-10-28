bad79caef48f90e752ef47ce81738d8914a59d1e
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-10-29 03:44:25 +0500

    adding the controller pattern

---
+++ controllers/auth.controller.js
@@ -0,0 +1,105 @@
+const createError = require('http-errors');
+const User = require('../models/user.model');
+const { authSchema } = require('../helpers/validationSchema');
+const {
+  signAccessToken,
+  signRefreshToken,
+  verifyRefreshToken,
+} = require('../helpers/jwtHelper');
+const client = require('../helpers/initRedis');
+
+module.exports = {
+  register: async (req, res, next) => {
+    try {
+      const result = await authSchema.validateAsync(req.body);
+
+      const { email, password } = result;
+
+      const doesExist = await User.findOne({ email });
+
+      if (doesExist) {
+        throw createError.Conflict(`${email} already exists`);
+      }
+
+      const user = new User({ email, password });
+
+      const savedUser = await user.save();
+      const accessToken = await signAccessToken(savedUser.id);
+      const refreshToken = await signRefreshToken(savedUser.id);
+
+      res.send({ accessToken, refreshToken });
+    } catch (error) {
+      if (error.isJoi === true) {
+        error.status = 422;
+      }
+      next(error);
+    }
+  },
+
+  login: async (req, res, next) => {
+    try {
+      const result = await authSchema.validateAsync(req.body);
+      const { email, password } = result;
+
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw createError.NotFound(`${email} does not register with us!`);
+      }
+
+      const isMatch = await user.isValidPassword(password);
+
+      if (!isMatch) {
+        throw createError.Unauthorized('Invalid password');
+      }
+
+      const accessToken = await signAccessToken(user.id);
+      const refreshToken = await signRefreshToken(user.id);
+
+      res.send({ accessToken, refreshToken });
+    } catch (error) {
+      if (error.isJoi === true) {
+        next(createError.BadRequest('Invalid credentials'));
+      }
+      next(error);
+    }
+  },
+
+  refereshToken: async (req, res, next) => {
+    try {
+      const { refreshToken } = req.body;
+      if (!refreshToken) {
+        throw createError.BadRequest();
+      }
+      const userId = await verifyRefreshToken(refreshToken);
+      const accessToken = await signAccessToken(userId);
+      const refToken = await signRefreshToken(userId);
+      res.send({ accessToken, refreshToken: refToken });
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  logout: async (req, res, next) => {
+    try {
+      const { refreshToken } = req.body;
+
+      if (!refreshToken) {
+        throw createError.BadRequest();
+      }
+
+      const userId = await verifyRefreshToken(refreshToken);
+
+      client.del(userId, (err, reply) => {
+        if (err) {
+          console.log(err.message);
+          throw createError.InternalServerError();
+        }
+        console.log(reply);
+        res.sendStatus(204);
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+};
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,108 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const createError = require('http-errors');
-const User = require('../models/user.model');
-const { authSchema } = require('../helpers/validationSchema');
+
 const {
-  signAccessToken,
-  signRefreshToken,
-  verifyRefreshToken,
-} = require('../helpers/jwtHelper');
-const { verify } = require('jsonwebtoken');
-const client = require('../helpers/initRedis');
+  register,
+  login,
+  refereshToken,
+  logout,
+} = require('../controllers/auth.controller');
 
-router.post('/register', async (req, res, next) => {
-  try {
-    const result = await authSchema.validateAsync(req.body);
+router.post('/register', register);
 
-    const { email, password } = result;
+router.post('/login', login);
 
-    const doesExist = await User.findOne({ email });
+router.post('/referesh-token', refereshToken);
 
-    if (doesExist) {
-      throw createError.Conflict(`${email} already exists`);
-    }
-
-    const user = new User({ email, password });
-
-    const savedUser = await user.save();
-    const accessToken = await signAccessToken(savedUser.id);
-    const refreshToken = await signRefreshToken(savedUser.id);
-
-    res.send({ accessToken, refreshToken });
-  } catch (error) {
-    if (error.isJoi === true) {
-      error.status = 422;
-    }
-    next(error);
-  }
-});
-
-router.post('/login', async (req, res, next) => {
-  try {
-    const result = await authSchema.validateAsync(req.body);
-    const { email, password } = result;
-
-    const user = await User.findOne({ email });
-
-    if (!user) {
-      throw createError.NotFound(`${email} does not register with us!`);
-    }
-
-    const isMatch = await user.isValidPassword(password);
-
-    if (!isMatch) {
-      throw createError.Unauthorized('Invalid password');
-    }
-
-    const accessToken = await signAccessToken(user.id);
-    const refreshToken = await signRefreshToken(user.id);
-
-    res.send({ accessToken, refreshToken });
-  } catch (error) {
-    if (error.isJoi === true) {
-      next(createError.BadRequest('Invalid credentials'));
-    }
-    next(error);
-  }
-});
-
-router.post('/referesh-token', async (req, res, next) => {
-  try {
-    const { refreshToken } = req.body;
-    if (!refreshToken) {
-      throw createError.BadRequest();
-    }
-    const userId = await verifyRefreshToken(refreshToken);
-    const accessToken = await signAccessToken(userId);
-    const refToken = await signRefreshToken(userId);
-    res.send({ accessToken, refreshToken: refToken });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.delete('/logout', async (req, res, next) => {
-  try {
-    const { refreshToken } = req.body;
-
-    if (!refreshToken) {
-      throw createError.BadRequest();
-    }
-
-    const userId = await verifyRefreshToken(refreshToken);
-
-    client.del(userId, (err, reply) => {
-      if (err) {
-        console.log(err.message);
-        throw createError.InternalServerError();
-      }
-      console.log(reply);
-      res.sendStatus(204);
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.delete('/logout', logout);
 
 module.exports = router;
